018d62d0e0d6252b5383df51ed6c4be4a2fefe09
AlfonsoCasajus <casajus.alfonso@gmail.com>
2024-12-26 11:25:44 -0300

    theme switch

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -1,59 +1,57 @@
 'use client'
-import { Navbar, NavbarContent, NavbarItem, Link, DropdownMenu, Avatar, Dropdown, DropdownItem, DropdownTrigger } from "@nextui-org/react";
+
+// Components
+import { Navbar, NavbarContent, NavbarItem, Avatar } from "@nextui-org/react";
 import { Switch } from "@nextui-org/react";
 
 // Icons
 import { FiSun } from "react-icons/fi";
 import { FaMoon } from "react-icons/fa";
 
+// Hooks
+import { useTheme } from "next-themes";
+import { useEffect, useState } from "react";
+
 export function NavBar() {
+
+  const [mounted, setMounted] = useState(false)
+  const { setTheme } = useTheme();
+
+  useEffect(() => setMounted(true), [])
+
+
+  if (!mounted) return;
+
   return (
-    <Navbar className="dark:bg-gray-700">
-      <NavbarContent as="div" justify="start">
-        <Dropdown placement="bottom-end">
-          <DropdownTrigger>
-            <Avatar
-              isBordered
-              as="button"
-              className="transition-transform"
-              color="secondary"
-              name="Jason Hughes"
-              size="sm"
-              src="https://i.pravatar.cc/150?u=a042581f4e29026704d"
-            />
-          </DropdownTrigger>
-          <DropdownMenu aria-label="Profile Actions" variant="flat">
-            <DropdownItem key="profile" className="h-14 gap-2">
-              <p className="font-semibold">Signed in as</p>
-              <p className="font-semibold">zoey@example.com</p>
-            </DropdownItem>
-            <DropdownItem key="settings">My Settings</DropdownItem>
-            <DropdownItem key="team_settings">Team Settings</DropdownItem>
-            <DropdownItem key="analytics">Analytics</DropdownItem>
-            <DropdownItem key="system">System</DropdownItem>
-            <DropdownItem key="configurations">Configurations</DropdownItem>
-            <DropdownItem key="help_and_feedback">Help & Feedback</DropdownItem>
-            <DropdownItem key="logout" color="danger">
-              Log Out
-            </DropdownItem>
-          </DropdownMenu>
-        </Dropdown>
+    <Navbar className="bg-gray-100 dark:bg-slate-800">
+      <NavbarContent justify="start">
+        <Avatar
+          isBordered
+          className="transition-transform"
+          color="primary"
+          name="Alfonso Casajus Ponce"
+          size="md"
+          src="https://i.pravatar.cc/150?u=a042581f4e29026704d"
+        />
+        <span>
+          <p className="text- dark:text-white">Alfonso Casajus Ponce</p>
+          <small className="dark:text-white">Software Developer</small>
+        </span>
       </NavbarContent>
-      <NavbarContent className="hidden sm:flex gap-4" justify="center">
+      <NavbarContent justify="center">
         <NavbarItem isActive>
-          <Link aria-current="page" href="#">
            Traduccion Ingles / Español
-          </Link>
         </NavbarItem>
       </NavbarContent>
       <NavbarContent justify="end">
-        <NavbarItem className="hidden lg:flex">
+        <NavbarItem>
           <Switch
             defaultSelected
             color="warning"
             endContent={<FaMoon  />}
             size="lg"
             startContent={<FiSun />}
+            onValueChange={(isSelected) => isSelected ? setTheme('light') : setTheme('dark')}
           />
         </NavbarItem>
       </NavbarContent>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,18 +3,16 @@ import { Hero } from "./components/sections/Hero"
 export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-          <Hero />
-        <section className="relative flex items-center justify-center">
-          <div>PROYECTOS</div>
-        </section>
-        <section className="relative flex items-center justify-center">
-          <div>Stack</div>
-        </section>
-        <section className="relative flex items-center justify-center">
-          <div>Experiencia</div>
-        </section>
-      </main>
+      <Hero />
+      <section className="relative flex items-center justify-center">
+        <div>PROYECTOS</div>
+      </section>
+      <section className="relative flex items-center justify-center">
+        <div>Stack</div>
+      </section>
+      <section className="relative flex items-center justify-center">
+        <div>Experiencia</div>
+      </section>
     </div>
   );
 }
