e0d65304aff3e9730ce5cad65e206934796063c9
AlfonsoCasajus <casajus.alfonso@gmail.com>
2024-12-26 10:01:54 -0300

    starting configs

---
+++ src/app/components/sections/Hero.tsx
@@ -0,0 +1,12 @@
+'use client'
+import { Button } from "@nextui-org/button";
+
+// Icons
+export function Hero() {
+  return (
+    <section className="relative flex items-center justify-center">
+		Hero
+		<Button color="primary">Button</Button>;
+	</section>
+  );
+};
