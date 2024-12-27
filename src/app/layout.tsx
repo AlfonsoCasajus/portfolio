280d0440aa1d5948b40e971aa1c7ef7805a6def8
AlfonsoCasajus <casajus.alfonso@gmail.com>
2024-12-27 11:00:56 -0300

    navbar file name change

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { Providers } from "./providers";
-import { NavBar } from "./components/Navbar"
+import { NavBar } from "./components/navigation/Navbar"
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
