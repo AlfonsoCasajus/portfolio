44a4b11645b256bf3dc79d9d70b05462845fa6b0
AlfonsoCasajus <casajus.alfonso@gmail.com>
2025-01-08 11:07:55 -0300

    mvp navbar

--- src/app/components/navigation/Navbar.tsx
+++ src/app/components/navigation/Navbar.tsx
@@ -1,7 +1,8 @@
 'use client'
 
 // Components
-import { Navbar, NavbarContent, NavbarItem, Switch, User, Select, SelectItem, Avatar } from "@nextui-org/react";
+import { Navbar, NavbarContent, NavbarItem, Switch, User } from "@nextui-org/react";
+// import { Select, SelectItem, Avatar } from "@nextui-org/react";
 
 // Icons
 import { FiSun } from "react-icons/fi";
@@ -14,70 +15,72 @@ import { useEffect, useState } from "react";
 export function NavBar() {
 
   const [mounted, setMounted] = useState(false)
-  // const [showNavbar, setShowNavbar] = useState(false)
-
+  
   const { theme, setTheme } = useTheme();
-
-  const languages = [
-    { key: "es", label: "Castellano", flag: 'ar' },
-    { key: "brt", label: "Ingles", flag: 'gb' }
-  ]
-
+  
+  // const languages = [
+  //   { key: "es", label: "Castellano", flag: 'ar' },
+  //   { key: "brt", label: "Ingles", flag: 'gb' }
+  // ]
+  
+  useEffect(() => setMounted(true), [])
+  
+  // Handle visibility
+  // const [showNavbar, setShowNavbar] = useState(false)
   // const handleScroll = () => {
   //   if (window.scrollY > 20) setShowNavbar(true)
   //   else setShowNavbar(false)
   // }
-
-  useEffect(() => setMounted(true), [])
   // useEffect(() => window.addEventListener('scroll', handleScroll), [])
 
-
   if (!mounted) return;
-
+    
   return (
-    <Navbar position="sticky" className="justify-around">
-      <NavbarContent justify="start">
-        <User
-          avatarProps={{
-            src: "profileAvatar.png",
-          }}
-          description="Web developer"
-          name="Alfonso Casajus"
-          className="dark:text-white"
-        />
-      </NavbarContent>
-
-      <NavbarContent justify="end">
-        <NavbarItem>
-          <Switch
-            isSelected={ theme === 'light' }
-            color="warning"
-            endContent={<FaMoon  />}
-            size="lg"
-            startContent={<FiSun />}
-            onValueChange={(isSelected) => isSelected ? setTheme('light') : setTheme('dark')}
+    <div>
+      <Navbar position="sticky" className="justify-around">
+        <NavbarContent justify="start">
+          <User
+            avatarProps={{
+              src: "profileAvatar.png",
+            }}
+            description="Web developer"
+            name="Alfonso Casajus"
+            className="dark:text-white"
           />
-        </NavbarItem>
-        <NavbarItem>
-          <div className="w-48">
-            <Select
-              defaultSelectedKeys={["es"]}
-              disallowEmptySelection
-            >
-              {
-                languages.map((language) => (
-                  <SelectItem
-                    key={language.key}
-                    startContent={<Avatar alt={`${language.label}`} className="w-6 h-6" src={`https://flagcdn.com/${language.flag}.svg`} />}
-                  >
-                    {language.label}
-                  </SelectItem>
-                ))
-              }
-            </Select>
-          </div>
-        </NavbarItem>
-      </NavbarContent>
-    </Navbar>
+        </NavbarContent>
+
+        <NavbarContent justify="end">
+          <NavbarItem>
+            <Switch
+              isSelected={ theme === 'light' }
+              color="warning"
+              endContent={<FaMoon  />}
+              size="lg"
+              startContent={<FiSun />}
+              onValueChange={(isSelected) => isSelected ? setTheme('light') : setTheme('dark')}
+            />
+          </NavbarItem>
+          {/* <NavbarItem>
+            <div className="w-48">
+              <Select
+                defaultSelectedKeys={["es"]}
+                disallowEmptySelection
+              >
+                {
+                  languages.map((language) => (
+                    <SelectItem
+                      key={language.key}
+                      startContent={<Avatar alt={`${language.label}`} className="w-6 h-6" src={`https://flagcdn.com/${language.flag}.svg`} />}
+                    >
+                      {language.label}
+                    </SelectItem>
+                  ))
+                }
+              </Select>
+            </div>
+          </NavbarItem> */}
+        </NavbarContent>
+      </Navbar>
+    </div>
   );
 };
